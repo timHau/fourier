91e06433b7fa89f4057614b5b1e433d8e713396a
timHau <tim.hau@hotmail.de>
2022-05-08 17:20:55 +0200

    Minor refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 mod utils;
 
 use num_complex::Complex64;
-use plotters::prelude::*;
-use std::{f64::consts::PI, ops::Range};
+use std::f64::consts::PI;
 
 pub fn dft(signal: &[Complex64]) -> Vec<Complex64> {
     let n = signal.len();
@@ -39,71 +38,10 @@ pub fn idft(spectrum: &[Complex64]) -> Vec<Complex64> {
     result
 }
 
-pub fn plot_signal(
-    signal: &[f64],
-    title: &str,
-    y_range: Range<f64>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new(title, (640, 420)).into_drawing_area();
-    root.fill(&WHITE)?;
-
-    let mut chart = ChartBuilder::on(&root)
-        .margin(10)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(0.0f64..(signal.len() as f64), y_range)?;
-
-    chart
-        .configure_mesh()
-        .x_labels(20)
-        .y_labels(10)
-        .x_desc("x")
-        .y_desc("y")
-        .disable_mesh()
-        .draw()?;
-
-    chart.draw_series(LineSeries::new(
-        signal.iter().enumerate().map(|(i, s)| (i as f64, *s)),
-        &RED,
-    ))?;
-
-    chart.configure_series_labels().draw()?;
-
-    root.present()?;
-    Ok(())
-}
-
-fn fftshift_1d(spectrum: &[f64]) -> Vec<f64> {
-    let n = spectrum.len();
-    let mut spectrum = spectrum.to_vec();
-    spectrum.rotate_right(n / 2);
-    spectrum
-}
-
-pub fn absolute_spectrum(spectrum: &[Complex64]) -> Vec<f64> {
-    spectrum.iter().map(|x| x.norm()).collect::<Vec<_>>()
-}
-
-pub fn plot_spectrum(
-    spectrum: &[Complex64],
-    title: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let n = spectrum.len();
-    // we are only in the absolute value of the spectrum
-    let mut abs_spectrum = absolute_spectrum(spectrum);
-    // we need to shift the spectrum to the center
-    abs_spectrum = fftshift_1d(&abs_spectrum);
-    // the spectrum is symmetric around 0.0 we only need the positive part
-    abs_spectrum = abs_spectrum.into_iter().take(n / 2).collect::<Vec<_>>();
-    abs_spectrum.reverse();
-
-    plot_signal(&abs_spectrum, title, -10.0..300.0)
-}
-
-pub fn fft_step(signal: &[f64], stride: usize) -> Vec<Complex64> {
+pub fn fft_step(signal: &[Complex64], stride: usize) -> Vec<Complex64> {
     let n = signal.len();
     if n == 1 {
-        return vec![Complex64::new(signal[0], 0.0)];
+        return signal.to_vec();
     } else {
         let mut even = vec![];
         even.reserve(n / 2);
@@ -130,17 +68,25 @@ pub fn fft_step(signal: &[f64], stride: usize) -> Vec<Complex64> {
     }
 }
 
-pub fn fft(signal: &[f64]) -> Vec<Complex64> {
+pub fn fft(signal: &[Complex64]) -> Vec<Complex64> {
     let n = signal.len();
     let mut signal = signal.to_vec();
     if !n.is_power_of_two() {
         // extend periodically
-        signal.resize(n.next_power_of_two(), 0.0);
+        signal.resize(n.next_power_of_two(), Complex64::default());
         for i in n..n.next_power_of_two() {
             signal[i] = signal[i % n];
         }
     }
     fft_step(&signal, 1)
+}
+
+pub fn fft_real(signal: &[f64]) -> Vec<Complex64> {
+    let complex_signal = signal
+        .iter()
+        .map(|x| Complex64::new(*x, 0.0))
+        .collect::<Vec<_>>();
+    fft(&complex_signal)
 }
 
 #[cfg(test)]
@@ -157,7 +103,7 @@ mod tests {
     }
 
     #[test]
-    fn example_plot_fft() {
+    fn example_plot_fft_real() {
         let n = 256;
         let mut signal = vec![0.0; n];
 
@@ -168,8 +114,8 @@ mod tests {
                 + f64::sin(2.0 * PI * freq_2 * (i as f64) / (n as f64));
         }
 
-        let spectrum = fft(&signal);
-        assert!(plot_spectrum(&spectrum, "spectrum_fft.png").is_ok());
+        let spectrum = fft_real(&signal);
+        assert!(utils::plot_spectrum(&spectrum, "spectrum_fft.png").is_ok());
     }
 
     #[test]
@@ -184,33 +130,10 @@ mod tests {
                 + f64::sin(2.0 * PI * freq_2 * (i as f64) / (n as f64));
         }
 
-        assert!(plot_signal(&signal, "signal.png", -2.0..2.0).is_ok());
+        assert!(utils::plot_signal(&signal, "signal.png", -2.0..2.0).is_ok());
 
         let spectrum = dft_real(&signal);
-        assert!(plot_spectrum(&spectrum, "spectrum_dft.png").is_ok());
-    }
-
-    #[test]
-    fn abs_spec() {
-        let spec = vec![
-            Complex64::new(0.0, 0.0),
-            Complex64::new(3.0, -4.0),
-            Complex64::new(-6.0, -8.0),
-            Complex64::new(15.0, 20.0),
-        ];
-        let abs_spec = absolute_spectrum(&spec);
-        let expect = vec![0.0, 5.0, 10.0, 25.0];
-        assert_eq!(abs_spec, expect);
-    }
-
-    #[test]
-    fn fftshift_1() {
-        let signal = [0.0, 1.0, 2.0, 3.0, 4.0, -5.0, -4.0, -3.0, -2.0, -1.0];
-        let shifted = fftshift_1d(&signal);
-        assert_eq!(
-            shifted,
-            [-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0]
-        );
+        assert!(utils::plot_spectrum(&spectrum, "spectrum_dft.png").is_ok());
     }
 
     #[test]
--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,94 @@
+use num_complex::Complex64;
+use plotters::prelude::*;
+use std::ops::Range;
 
-pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
-    // `set_panic_hook` function at least once during initialization, and then
-    // we will get better error messages if our code ever panics.
-    //
-    // For more details see
-    // https://github.com/rustwasm/console_error_panic_hook#readme
-    #[cfg(feature = "console_error_panic_hook")]
-    console_error_panic_hook::set_once();
+pub fn plot_signal(
+    signal: &[f64],
+    title: &str,
+    y_range: Range<f64>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(title, (640, 420)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(10)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(0.0f64..(signal.len() as f64), y_range)?;
+
+    chart
+        .configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .x_desc("x")
+        .y_desc("y")
+        .disable_mesh()
+        .draw()?;
+
+    chart.draw_series(LineSeries::new(
+        signal.iter().enumerate().map(|(i, s)| (i as f64, *s)),
+        &RED,
+    ))?;
+
+    chart.configure_series_labels().draw()?;
+
+    root.present()?;
+    Ok(())
+}
+
+pub fn fftshift_1d(spectrum: &[f64]) -> Vec<f64> {
+    let n = spectrum.len();
+    let mut spectrum = spectrum.to_vec();
+    spectrum.rotate_right(n / 2);
+    spectrum
+}
+
+pub fn absolute_spectrum(spectrum: &[Complex64]) -> Vec<f64> {
+    spectrum.iter().map(|x| x.norm()).collect::<Vec<_>>()
+}
+
+pub fn plot_spectrum(
+    spectrum: &[Complex64],
+    title: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let n = spectrum.len();
+    // we are only in the absolute value of the spectrum
+    let mut abs_spectrum = absolute_spectrum(spectrum);
+    // we need to shift the spectrum to the center
+    abs_spectrum = fftshift_1d(&abs_spectrum);
+    // the spectrum is symmetric around 0.0 we only need the positive part
+    abs_spectrum = abs_spectrum.into_iter().take(n / 2).collect::<Vec<_>>();
+    abs_spectrum.reverse();
+
+    plot_signal(&abs_spectrum, title, -10.0..300.0)
+}
+
+#[cfg(test)]
+mod tests {
+    use num_complex::Complex64;
+    use super::*;
+
+    #[test]
+    fn abs_spec() {
+        let spec = vec![
+            Complex64::new(0.0, 0.0),
+            Complex64::new(3.0, -4.0),
+            Complex64::new(-6.0, -8.0),
+            Complex64::new(15.0, 20.0),
+        ];
+        let abs_spec = absolute_spectrum(&spec);
+        let expect = vec![0.0, 5.0, 10.0, 25.0];
+        assert_eq!(abs_spec, expect);
+    }
+
+  #[test]
+    fn fftshift_1() {
+        let signal = [0.0, 1.0, 2.0, 3.0, 4.0, -5.0, -4.0, -3.0, -2.0, -1.0];
+        let shifted = fftshift_1d(&signal);
+        assert_eq!(
+            shifted,
+            [-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0]
+        );
+    }
+
 }
