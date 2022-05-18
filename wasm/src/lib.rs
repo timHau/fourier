e1d2cd2de79fd69e21ea430300b34cc6b53a8f3d
timHau <tim.hau@hotmail.de>
2022-05-18 22:06:27 +0200

    Fix ndarray import

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
 use fourier::fft2_real;
-use ndarray::Array1;
+use ndarray::array;
 use wasm_bindgen::prelude::*;
 
 #[cfg(feature = "wee_alloc")]
@@ -12,11 +12,11 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn greet() {
     utils::set_panic_hook();
 
-    let signal = ndarray![
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+    let signal = array![
+        [1.0, 2.0, 3.0, 4.0],
+        [1.0, 2.0, 3.0, 4.0],
+        [1.0, 2.0, 3.0, 4.0],
+        [1.0, 2.0, 3.0, 4.0],
     ];
 
     let res = fft2_real(&signal);
